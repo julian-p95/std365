3e9eee7d8b3bc9a4c0c410d934a339973f6673f7
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-07 02:09:03 +0200

    Update test16.py

--- test16.py
+++ test16.py
@@ -1,7 +1,47 @@
-# ... (même code pour les imports et les fonctions)
+import streamlit as st
+import pandas as pd
+import networkx as nx
+from pyvis.network import Network
+from collections import Counter
+import random
+
+# Générer une couleur aléatoire
+def random_color():
+    return "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+# Chargement des fichiers Excel
+uploaded_file_erp = st.file_uploader("Upload erp_all_table_relations_finalV2.xlsx", type=['xlsx'])
+uploaded_file_d365fo = st.file_uploader("Upload D365FO.xlsx", type=['xlsx'])
+uploaded_file_field_list = st.file_uploader("Upload Table and Field List.xlsx", type=['xlsx'])
 
 if uploaded_file_erp and uploaded_file_d365fo and uploaded_file_field_list:
-    # ... (même code pour le chargement des fichiers et le filtrage initial)
+
+    # Lecture des données
+    erp_relations_df = pd.read_excel(uploaded_file_erp)
+    d365_table_df = pd.read_excel(uploaded_file_d365fo, sheet_name='D365 Table')
+    field_list_df = pd.read_excel(uploaded_file_field_list, sheet_name='Field List')
+
+    # Comptage des relations
+    total_counter = Counter(erp_relations_df['Table Parent']) + Counter(erp_relations_df['Table Enfant'])
+
+    # Conversion en DataFrame
+    table_counts = pd.DataFrame.from_dict(total_counter, orient='index', columns=['Total Associations']).reset_index()
+    table_counts.rename(columns={'index': 'Table'}, inplace=True)
+
+    # Jointure avec d365_table_df
+    table_counts = table_counts.merge(d365_table_df[['Table name', 'App module']], left_on='Table', right_on='Table name', how='left')
+
+    # Suppression des NaN
+    table_counts.dropna(subset=['App module'], inplace=True)
+
+    # Liste des modules
+    app_modules = table_counts['App module'].unique().tolist()
+
+    # Sélection du module
+    app_module = st.selectbox('App Module:', app_modules)
+
+    # Filtrage des tables du module sélectionné
+    filtered_tables = table_counts[table_counts['App module'] == app_module]
 
     # Nombre de tables à afficher
     num_tables = st.slider('Nombre de tables:', min_value=1, max_value=len(filtered_tables), value=min(10, len(filtered_tables)))
@@ -9,7 +49,7 @@ if uploaded_file_erp and uploaded_file_d365fo and uploaded_file_field_list:
     # Tables avec le plus de relations
     top_tables = filtered_tables.nlargest(num_tables, 'Total Associations')['Table'].tolist()
 
-    # Filtrage des relations pour inclure les tables liées à top_tables
+    # Filtrage des relations
     filtered_relations = erp_relations_df[
         erp_relations_df['Table Parent'].isin(top_tables) | 
         erp_relations_df['Table Enfant'].isin(top_tables)
@@ -18,7 +58,7 @@ if uploaded_file_erp and uploaded_file_d365fo and uploaded_file_field_list:
     # Création du graphe
     G = nx.Graph()
     for _, row in filtered_relations.iterrows():
-        G.add_edge(row['Table Parent'], row['Table Enfant'], app_module=row['App module'])
+        G.add_edge(row['Table Parent'], row['Table Enfant'])
 
     # Création du graphe PyVis
     net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="black")
@@ -42,8 +82,6 @@ if uploaded_file_erp and uploaded_file_d365fo and uploaded_file_field_list:
     for module, color in color_map.items():
         st.write(f"{module} : {color}")
 
-    # Affichage du tableau
+    # Tableau des tables
     table_to_show = filtered_tables.nlargest(num_tables, 'Total Associations')[['Table', 'Total Associations', 'App module']]
     st.table(table_to_show.sort_values('Total Associations', ascending=False))
-
-# Fin du code
