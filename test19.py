1d391bf8b11cb3578d5ac807c95e524c4171e18a
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-07 03:34:06 +0200

    Update test19.py

--- test19.py
+++ test19.py
@@ -11,7 +11,7 @@ def random_color():
     return "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 # Initialisation de Streamlit
-st.title('Visualisation des Relations de Tables dans l\'ERP D365')
+st.title("Visualisation des Relations de Tables dans l'ERP D365")
 
 # Téléchargement des fichiers Excel
 uploaded_file_erp = st.file_uploader("Télécharger erp_all_table_relations_finalV2.xlsx", type=['xlsx'])
@@ -19,7 +19,7 @@ uploaded_file_d365fo = st.file_uploader("Télécharger D365FO.xlsx", type=['xlsx
 uploaded_file_field_list = st.file_uploader("Télécharger Table and Field List.xlsx", type=['xlsx'])
 
 # Vérification si tous les fichiers sont téléchargés
-if uploaded_file_erp is not None and uploaded_file_d365fo is not None and uploaded_file_field_list is not None:
+if all([uploaded_file_erp, uploaded_file_d365fo, uploaded_file_field_list]):
     
     # Lecture des fichiers Excel
     erp_all_table_relations = pd.read_excel(uploaded_file_erp, sheet_name='Sheet1')
@@ -57,26 +57,22 @@ if uploaded_file_erp is not None and uploaded_file_d365fo is not None and upload
         (erp_all_table_relations['Table Enfant'].isin(top_tables))
     ]
     
-    # Création du graphe avec NetworkX
-    G = nx.Graph()
-    for _, row in filtered_relations.iterrows():
-        G.add_edge(row['Table Parent'], row['Table Enfant'], title=row['Lien 1'])
-    
     # Création du graphe avec PyVis
     net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="black")
     
-    # Ajout des nœuds et des arêtes
+    # Ajout des nœuds
     for node in top_tables:
         net.add_node(node)
     
+    # Ajout des arêtes (en s'assurant que les nœuds existent)
     for _, row in filtered_relations.iterrows():
-        net.add_edge(row['Table Parent'], row['Table Enfant'])
+        if row['Table Parent'] in top_tables and row['Table Enfant'] in top_tables:
+            net.add_edge(row['Table Parent'], row['Table Enfant'])
     
     # Ajout des couleurs et des informations sur les colonnes
     for node in top_tables:
         node_info = df_total_counter[df_total_counter['Table'] == node]
         if not node_info.empty:
-            node_module = node_info['App module'].values[0]
             node_color = random_color()
             net.get_node(node)['color'] = node_color
             
