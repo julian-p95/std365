7e1c090179459ea98e7c0466dccbde2d3ef976e3
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-07 02:40:01 +0200

    Update test17.py

--- test17.py
+++ test17.py
@@ -1,4 +1,3 @@
-# Importation des bibliothèques
 import streamlit as st
 import pandas as pd
 import networkx as nx
@@ -6,16 +5,16 @@ from pyvis.network import Network
 from collections import Counter
 import random
 
-# Génération d'une couleur aléatoire
+# Fonction pour générer une couleur aléatoire
 def random_color():
     return "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-# Téléchargement des fichiers Excel
+# Upload des fichiers Excel
 uploaded_file_erp = st.file_uploader("Upload erp_all_table_relations_finalV2.xlsx", type=['xlsx'])
 uploaded_file_d365fo = st.file_uploader("Upload D365FO.xlsx", type=['xlsx'])
 uploaded_file_field_list = st.file_uploader("Upload Table and Field List.xlsx", type=['xlsx'])
 
-# Vérification que tous les fichiers sont bien téléchargés
+# Vérification que tous les fichiers sont chargés
 if uploaded_file_erp and uploaded_file_d365fo and uploaded_file_field_list:
     erp_relations_df = pd.read_excel(uploaded_file_erp)
     d365_table_df = pd.read_excel(uploaded_file_d365fo, sheet_name='D365 Table')
@@ -55,31 +54,28 @@ if uploaded_file_erp and uploaded_file_d365fo and uploaded_file_field_list:
         erp_relations_df['Table Enfant'].isin(top_tables)
     ]
 
-    # Création du graphe
+    # Création du graphe avec NetworkX
     G = nx.Graph()
     for _, row in filtered_relations.iterrows():
         G.add_edge(row['Table Parent'], row['Table Enfant'])
 
-    # Graphe PyVis
+    # Création du graphe avec PyVis
     net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="black")
     net.from_nx(G)
 
-    # Couleurs par module
-    color_map = {module: random_color() for module in app_modules}
+    # Assignation des couleurs par module d'application
+    color_map = {}
+    for app_module in app_modules:
+        color_map[app_module] = random_color()
+
     for node in G.nodes():
         node_info = table_counts.loc[table_counts['Table'] == node, 'App module']
         if not node_info.empty:
-            net.get_node(node)['color'] = color_map.get(node_info.iloc[0], "#000000")
+            node_module = node_info.iloc[0]
+            net.get_node(node)['color'] = color_map.get(node_module, "#000000")
 
-        # Ajout des infobulles pour les champs des tables
-        fields = field_list_df[field_list_df['TABLE_NAME'] == node]['COLUMN_NAME'].tolist()
-        fields_str = ', '.join(fields)
-        net.get_node(node)['title'] = f'Fields: {fields_str}'
-
-    # Sauvegarde du graphe dans un fichier HTML
+    # Sauvegarde et affichage du graphique dans Streamlit
     net.save_graph("temp.html")
-
-    # Lecture et affichage du fichier HTML dans Streamlit
     with open("temp.html", 'r', encoding='utf-8') as f:
         source_code = f.read()
     st.components.v1.html(source_code, height=800)
@@ -88,3 +84,6 @@ if uploaded_file_erp and uploaded_file_d365fo and uploaded_file_field_list:
     st.write("Légende des couleurs:")
     for app_module, color in color_map.items():
         st.write(f"{app_module} : {color}")
+
+    # Afficher le tableau des tables
+    st.table(filtered_tables[['Table', 'Total Associations']].sort_values('Total Associations', ascending=False))
