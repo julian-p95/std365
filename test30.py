e82cf9795f497464af82f020e6b5aabe2bd0fc77
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-08 23:02:29 +0200

    Update test30.py

--- test30.py
+++ test30.py
@@ -1,9 +1,7 @@
 # Importation des bibliothèques
 import pandas as pd
 import random
-from pyvis.network import Network
 from collections import Counter
-import streamlit as st
 # import de pyvis et streamlit à faire dans votre environnement
 
 # Génération d'une couleur aléatoire
@@ -11,13 +9,15 @@ def random_color():
     return "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 # Lecture des fichiers Excel
-erp_relations = pd.read_excel("erp_all_table_relations_finalV2.xlsx", sheet_name='Sheet1')
-d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
+erp_relations = pd.read_excel("/mnt/data/erp_all_table_relations_finalV2.xlsx", sheet_name='Sheet1')
+d365_tables = pd.read_excel("/mnt/data/D365FO.xlsx", sheet_name='D365 Table')
+field_list = pd.read_excel("/mnt/data/Table and Field List.xlsx", sheet_name='Field List')  # Réintégration du chargement
 
-# Conversion en majuscules pour assurer la correspondance
+# Conversion en majuscules
 erp_relations['Table Parent'] = erp_relations['Table Parent'].astype(str).str.upper()
 erp_relations['Table Enfant'] = erp_relations['Table Enfant'].astype(str).str.upper()
 d365_tables['Table name'] = d365_tables['Table name'].astype(str).str.upper()
+field_list['TABLE_NAME'] = field_list['TABLE_NAME'].astype(str).str.upper()
 
 # Dictionnaire de couleurs pour chaque module d'application
 app_module_colors = {module: random_color() for module in d365_tables['App module'].unique()}
@@ -63,11 +63,14 @@ else:
 # Création du graphe
 net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="black")
 
-# Ajout des nœuds
+# Ajout des nœuds avec informations sur les relations
 graphed_tables = set()
 for table in top_tables:
     color = app_module_colors.get(app_module, random_color())
-    net.add_node(table, color=color)
+    related_tables = filtered_relations[(filtered_relations['Table Parent'] == table) | (filtered_relations['Table Enfant'] == table)]
+    relationships = ["{} -> {}".format(row['Table Parent'], row['Table Enfant']) for _, row in related_tables.iterrows()]
+    title_str = "Table: {}\nApp Module: {}\nRelations:\n{}".format(table, app_module, "\n".join(relationships))
+    net.add_node(table, title=title_str, color=color)
     graphed_tables.add(table)
 
 # Ajout des arêtes
@@ -94,3 +97,8 @@ net.save_graph("temp.html")
 with open("temp.html", 'r', encoding='utf-8') as f:
     source_code = f.read()
 st.components.v1.html(source_code, height=800)
+
+# Tableau pour la sélection de la table et l'affichage des champs
+table_choice = st.selectbox('Choisissez une table pour afficher ses champs:', list(graphed_tables))
+table_fields = field_list[field_list['TABLE_NAME'] == table_choice]
+st.table(table_fields[['COLUMN_NAME', 'DATA_TYPE']])
