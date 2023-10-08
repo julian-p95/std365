80e0b3b8ba5b949de0e5b208a5f3fd588d7fe30a
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-09 00:39:29 +0200

    Update test30.py

--- test30.py
+++ test30.py
@@ -11,8 +11,8 @@ def random_color():
 
 # Lecture des fichiers Excel
 erp_relations = pd.read_excel("erp_all_table_relations_finalV2.xlsx", sheet_name='Sheet1')
-d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
-field_list = pd.read_excel("Table and Field List.xlsx", sheet_name='Field List')
+d365_tables = pd.read_excel("data/D365FO.xlsx", sheet_name='D365 Table')
+field_list = pd.read_excel("data/Table and Field List.xlsx", sheet_name='Field List')
 
 # Conversion en majuscules
 erp_relations['Table Parent'] = erp_relations['Table Parent'].astype(str).str.upper()
@@ -20,12 +20,8 @@ erp_relations['Table Enfant'] = erp_relations['Table Enfant'].astype(str).str.up
 d365_tables['Table name'] = d365_tables['Table name'].astype(str).str.upper()
 field_list['TABLE_NAME'] = field_list['TABLE_NAME'].astype(str).str.upper()
 
-# Suppression des valeurs NaN dans 'App module' et tri
-d365_tables['App module'].fillna('Inconnu', inplace=True)
-app_modules = sorted(d365_tables['App module'].unique().tolist())
-
-# Dictionnaire de couleurs
-app_module_colors = {module: random_color() for module in app_modules}
+# Dictionnaire de couleurs pour chaque module d'application
+app_module_colors = {module: random_color() for module in d365_tables['App module'].unique()}
 
 # Comptage des occurrences
 total_counter = Counter(erp_relations['Table Parent']) + Counter(erp_relations['Table Enfant'])
@@ -37,9 +33,9 @@ df_total_counter.rename(columns={'index': 'Table'}, inplace=True)
 # Jointure pour récupérer les modules d'application
 df_total_counter = df_total_counter.merge(d365_tables[['Table name', 'App module']], left_on='Table', right_on='Table name', how='left')
 
-# Module de recherche et sélection du module d'application
-search_term_module = st.text_input('Rechercher un module:')
-app_module = st.selectbox('Module d\'Application:', [module for module in app_modules if search_term_module.lower() in module.lower()])
+# Sélection du module d'application
+app_modules = sorted(d365_tables['App module'].unique().tolist())
+app_module = st.selectbox('Module d\'Application:', app_modules, format_func=lambda x: x if x else "None")
 
 # Filtrage des tables pour le module sélectionné
 filtered_tables = df_total_counter[df_total_counter['App module'] == app_module]
@@ -48,38 +44,36 @@ filtered_tables = df_total_counter[df_total_counter['App module'] == app_module]
 num_tables = st.slider('Nombre de tables:', min_value=1, max_value=len(filtered_tables), value=10)
 
 # Tables avec le plus grand nombre de relations
-top_tables = sorted(filtered_tables.nlargest(num_tables, 'Total Associations')['Table'].tolist())
+top_tables = filtered_tables.nlargest(num_tables, 'Total Associations')['Table'].tolist()
 
-# Filtrage des relations pour inclure seulement ces tables
-filtered_relations = erp_relations[erp_relations['Table Parent'].isin(top_tables) & erp_relations['Table Enfant'].isin(top_tables)]
+# Filtrage des relations pour inclure seulement les tables top
+filtered_relations = erp_relations[
+    erp_relations['Table Parent'].isin(top_tables) | 
+    erp_relations['Table Enfant'].isin(top_tables)
+]
 
 # Création du graphe
 net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="black")
 
-# Ajout des nœuds et des arêtes
+# Ajout des nœuds avec informations supplémentaires
 graphed_tables = set()
+for table in top_tables:
+    table_info = d365_tables[d365_tables['Table name'] == table].iloc[0]
+    existing_columns = [col for col in ['Table name', 'Table label', 'Table group', 'Table type', 'App module', 'Form reference', 'SysTable', 'View', 'Global'] if col in table_info.index]
+    title_str = "\n".join([f"{col}: {table_info[col]}" for col in existing_columns])
+    
+    color = app_module_colors.get(app_module, random_color())
+    net.add_node(table, title=title_str, color=color)
+    graphed_tables.add(table)
+
+# Ajout des arêtes avec informations sur les relations
 for _, row in filtered_relations.iterrows():
     parent = row['Table Parent']
     child = row['Table Enfant']
-    relation_str = row['Lien 1']
+    relation = row['Lien 1']
     
-    table_info = d365_tables[d365_tables['Table name'] == parent].iloc[0]
-    title_str = "\n".join([f"{col}: {table_info[col]}" for col in ['Table name', 'Table label', 'Table group', 'Table type', 'App module', 'Form reference', 'SysTable', 'View', 'Global']])
-    
-    if parent not in graphed_tables:
-        color = app_module_colors.get(app_module, random_color())
-        net.add_node(parent, title=title_str, color=color)
-        graphed_tables.add(parent)
-    
-    table_info = d365_tables[d365_tables['Table name'] == child].iloc[0]
-    title_str = "\n".join([f"{col}: {table_info[col]}" for col in ['Table name', 'Table label', 'Table group', 'Table type', 'App module', 'Form reference', 'SysTable', 'View', 'Global']])
-    
-    if child not in graphed_tables:
-        color = app_module_colors.get(app_module, random_color())
-        net.add_node(child, title=title_str, color=color)
-        graphed_tables.add(child)
-    
-    net.add_edge(parent, child, title=relation_str)
+    if parent in graphed_tables and child in graphed_tables:
+        net.add_edge(parent, child, title=relation)
 
 # Affichage du graphe
 net.save_graph("temp.html")
@@ -87,8 +81,7 @@ with open("temp.html", 'r', encoding='utf-8') as f:
     source_code = f.read()
 st.components.v1.html(source_code, height=800)
 
-# Module de recherche et tableau pour afficher les champs
-search_term_table = st.text_input('Rechercher une table:')
-table_choice = st.selectbox('Choisissez une table pour afficher ses champs:', [table for table in sorted(list(graphed_tables)) if search_term_table.lower() in table.lower()])
-table_fields = field_list[field_list['TABLE_NAME'] == table_choice]
+# Tableau pour la sélection de la table et l'affichage des champs
+table_choice = st.selectbox('Choisissez une table pour afficher ses champs:', sorted(list(graphed_tables)))
+table_fields = field_list[field_list['TABLE_NAME'] == table_choice.upper()]
 st.table(table_fields[['COLUMN_NAME', 'DATA_TYPE']])
