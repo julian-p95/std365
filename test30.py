185d9a298fd3af969799ed0853bea6b80a6243e0
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-09 00:46:38 +0200

    Update test30.py

--- test30.py
+++ test30.py
@@ -5,7 +5,7 @@ from pyvis.network import Network
 from collections import Counter
 import streamlit as st
 
-# Génération d'une couleur aléatoire
+# Fonction pour générer une couleur aléatoire
 def random_color():
     return "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
@@ -21,58 +21,38 @@ d365_tables['Table name'] = d365_tables['Table name'].astype(str).str.upper()
 field_list['TABLE_NAME'] = field_list['TABLE_NAME'].astype(str).str.upper()
 
 # Dictionnaire de couleurs pour chaque module d'application
-app_module_colors = {module: random_color() for module in d365_tables['App module'].unique()}
+app_module_colors = {module: random_color() for module in d365_tables['App module'].dropna().unique()}
 
 # Comptage des occurrences
 total_counter = Counter(erp_relations['Table Parent']) + Counter(erp_relations['Table Enfant'])
 
-# Conversion en DataFrame
-df_total_counter = pd.DataFrame.from_dict(total_counter, orient='index', columns=['Total Associations']).reset_index()
-df_total_counter.rename(columns={'index': 'Table'}, inplace=True)
-
-# Jointure pour récupérer les modules d'application
-df_total_counter = df_total_counter.merge(d365_tables[['Table name', 'App module']], left_on='Table', right_on='Table name', how='left')
-
 # Sélection du module d'application
-app_modules = sorted(d365_tables['App module'].unique().tolist())
-app_module = st.selectbox('Module d\'Application:', app_modules, format_func=lambda x: x if x else "None")
+app_modules = sorted(d365_tables['App module'].dropna().unique().tolist())
+app_module = st.selectbox('Module d\'Application:', app_modules, format_func=lambda x: x if x else "N/A")
 
 # Filtrage des tables pour le module sélectionné
-filtered_tables = df_total_counter[df_total_counter['App module'] == app_module]
+filtered_tables = d365_tables[d365_tables['App module'] == app_module]
 
-# Slider pour le nombre de tables
-num_tables = st.slider('Nombre de tables:', min_value=1, max_value=len(filtered_tables), value=10)
-
-# Tables avec le plus grand nombre de relations
-top_tables = filtered_tables.nlargest(num_tables, 'Total Associations')['Table'].tolist()
-
-# Filtrage des relations pour inclure seulement les tables top
-filtered_relations = erp_relations[
-    erp_relations['Table Parent'].isin(top_tables) | 
-    erp_relations['Table Enfant'].isin(top_tables)
-]
+# Tables graphées, limitées aux tables de l'app module sélectionné
+top_tables = sorted(filtered_tables['Table name'].tolist())
 
 # Création du graphe
 net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="black")
 
-# Ajout des nœuds avec informations supplémentaires
-graphed_tables = set()
+# Ajout des nœuds avec informations D365FO
 for table in top_tables:
-    table_info = d365_tables[d365_tables['Table name'] == table].iloc[0]
-    existing_columns = [col for col in ['Table name', 'Table label', 'Table group', 'Table type', 'App module', 'Form reference', 'SysTable', 'View', 'Global'] if col in table_info.index]
-    title_str = "\n".join([f"{col}: {table_info[col]}" for col in existing_columns])
-    
     color = app_module_colors.get(app_module, random_color())
+    table_info = d365_tables[d365_tables['Table name'] == table].iloc[0]
+    title_str = "\n".join([f"{col}: {table_info[col]}" for col in table_info.index if pd.notna(table_info[col])])
     net.add_node(table, title=title_str, color=color)
-    graphed_tables.add(table)
 
-# Ajout des arêtes avec informations sur les relations
+# Ajout des arêtes avec leurs attributs
+filtered_relations = erp_relations[(erp_relations['Table Parent'].isin(top_tables)) | (erp_relations['Table Enfant'].isin(top_tables))]
 for _, row in filtered_relations.iterrows():
     parent = row['Table Parent']
     child = row['Table Enfant']
     relation = row['Lien 1']
-    
-    if parent in graphed_tables and child in graphed_tables:
+    if parent in top_tables and child in top_tables:
         net.add_edge(parent, child, title=relation)
 
 # Affichage du graphe
@@ -82,6 +62,6 @@ with open("temp.html", 'r', encoding='utf-8') as f:
 st.components.v1.html(source_code, height=800)
 
 # Tableau pour la sélection de la table et l'affichage des champs
-table_choice = st.selectbox('Choisissez une table pour afficher ses champs:', sorted(list(graphed_tables)))
-table_fields = field_list[field_list['TABLE_NAME'] == table_choice.upper()]
+table_choice = st.selectbox('Choisissez une table pour afficher ses champs:', sorted(top_tables))
+table_fields = field_list[field_list['TABLE_NAME'] == table_choice]
 st.table(table_fields[['COLUMN_NAME', 'DATA_TYPE']])
