eb17a3d33a9bb08acd303a8f3d96e83b32969126
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-10 20:08:47 +0200

    Update main4.py

--- main4.py
+++ main4.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 # Lecture des fichiers Excel
 erp_relations = pd.read_excel("erp_all_table_relations_finalV2.xlsx", sheet_name='Sheet1')
-d365_tables = pd.read_excel("/D365FO.xlsx", sheet_name='D365 Table')
+d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
 
 # Sélection de l'App module
 selected_app_module = st.selectbox('Sélectionnez un App module:', d365_tables['App module'].unique().tolist())
