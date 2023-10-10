bdb43d4a85b267234d6b2c62766561efe30c6cbc
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-10 21:27:50 +0200

    Update main3.py

--- main3.py
+++ main3.py
@@ -8,22 +8,33 @@ st.title("Analyse des Tables ERP")
 # Lecture du fichier Excel
 d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
 
-# Correction du nom de la colonne
+# Gestion des valeurs NaN
+d365_tables['App module'].fillna("Non spécifié", inplace=True)
+d365_tables['Table group'].fillna("Non spécifié", inplace=True)
 d365_tables['Tabletype'].fillna("Non spécifié", inplace=True)
 
-# Graphique avec App modules sélectionnés
+# Filtrage des App modules
 filtered_tables = d365_tables[~d365_tables['App module'].isin(['SystemAdministration', 'General', 'Non spécifié'])]
-app_module_counts = filtered_tables['App module'].value_counts()
-fig1, ax1 = plt.subplots()
-app_module_counts.plot(kind='bar', ax=ax1)
-plt.title('Répartition des App modules (filtrés)')
-plt.xlabel('App module')
-plt.ylabel('Nombre de tables')
-st.pyplot(fig1)
 
-# Graphique avec tous les App modules
+# Répartition par App module
+app_module_counts = filtered_tables['App module'].value_counts()
+
+# 5 types de graphiques
+fig_types = ['bar', 'barh', 'pie', 'area', 'line']
+for ftype in fig_types:
+    fig, ax = plt.subplots()
+    app_module_counts.plot(kind=ftype, ax=ax)
+    plt.title(f'Répartition des App modules - {ftype}')
+    plt.xlabel('App module')
+    plt.ylabel('Nombre de tables')
+    st.pyplot(fig)
+
+# Graphique pour tous les App modules
 all_app_module_counts = d365_tables['App module'].value_counts()
-fig2, ax2 = plt.subplots()
-all_app_module_counts.plot(kind='pie', ax=ax2, autopct='%1.1f%%')
-plt.title('Répartition de tous les App modules')
-st.pyplot(fig2)
+for ftype in fig_types:
+    fig, ax = plt.subplots()
+    all_app_module_counts.plot(kind=ftype, ax=ax, autopct='%1.1f%%' if ftype == 'pie' else None)
+    plt.title(f'Poids de chaque App module - {ftype}')
+    plt.xlabel('App module')
+    plt.ylabel('Nombre de tables')
+    st.pyplot(fig)
