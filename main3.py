2132db5b87328427e5f5cff3dd2ca26f0ced6b15
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-10 20:04:43 +0200

    f

--- main3.py
+++ main3.py
@@ -9,12 +9,14 @@ st.title("Analyse des Tables ERP")
 d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
 
 # Gestion des valeurs NaN
-d365_tables.dropna(subset=['App module'], inplace=True)
+d365_tables = d365_tables.dropna(subset=['App module'])  # Supprime les lignes où 'App module' est NaN
+d365_tables['Table group'].fillna("Non spécifié", inplace=True)
+d365_tables['Tabletype'].fillna("Non spécifié", inplace=True)
 
 # Création d'une nouvelle colonne pour App module - Table group - Table type
-d365_tables['AppModule-TableGroup-TableType'] = d365_tables['App module'] + " - " + d365_tables['Table group'].astype(str) + " - " + d365_tables['Table type'].astype(str)
+d365_tables['AppModule-TableGroup-TableType'] = d365_tables['App module'] + " - " + d365_tables['Table group'] + " - " + d365_tables['Tabletype']
 
-# Filtrage pour enlever 'Non spécifié'
+# Suppression de l'App module "Non spécifié"
 d365_tables = d365_tables[d365_tables['App module'] != 'Non spécifié']
 
 # Graphique à barres horizontales pour AppModule-TableGroup-TableType
@@ -24,5 +26,5 @@ grouped_counts.plot(kind='barh', ax=ax)
 plt.title('Répartition des App module - Table group - Table type')
 plt.xlabel('Nombre de tables')
 plt.ylabel('App module - Table group - Table type')
-plt.subplots_adjust(left=0.25)
+ax.bar_label(ax.containers[0])
 st.pyplot(fig)
--- main4.py
+++ main4.py
@@ -2,18 +2,19 @@ import pandas as pd
 import streamlit as st
 
 # Lecture des fichiers Excel
-erp_relations = pd.read_excel("erp_all_table_relations_finalV2.xlsx", sheet_name='Sheet1')
-d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
+erp_relations = pd.read_excel("/path/to/erp_all_table_relations_finalV2.xlsx", sheet_name='Sheet1')
+d365_tables = pd.read_excel("/path/to/D365FO.xlsx", sheet_name='D365 Table')
 
 # Sélection de l'App module
 selected_app_module = st.selectbox('Sélectionnez un App module:', d365_tables['App module'].unique().tolist())
 
 # Tableau des relations par App module
-filtered_relations = erp_relations[erp_relations['App Module Parent'] == selected_app_module]
-relation_summary = filtered_relations.groupby('App Module Enfant').size().reset_index(name='Total Relations').sort_values(by='Total Relations', ascending=False)
+filtered_relations = erp_relations[(erp_relations['App Module Parent'] == selected_app_module)]
+relation_summary = filtered_relations.groupby('App Module Enfant').size().reset_index(name='Total Relations')
+relation_summary = relation_summary.sort_values(by='Total Relations', ascending=False)
 st.table(relation_summary)
 
 # Tableau des tables par App module
 filtered_tables = d365_tables[d365_tables['App module'] == selected_app_module]
-top_tables = filtered_tables.nlargest(20, 'Total Relations')
-st.table(top_tables[['Table name', 'Table label', 'Table group', 'Table type', 'Total Relations']])
+sorted_tables = filtered_tables.sort_values(by='Total Relations', ascending=False).head(20)  # Affiche les 20 premières tables triées par 'Total Relations'
+st.table(sorted_tables[['Table name', 'Table label', 'Table group', 'Tabletype']])
