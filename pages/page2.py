9f801f242a192fcc9688d68a3c1722bf03338a9c
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-10 15:49:33 +0200

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -1,20 +1,19 @@
-# page2.py
-# Page pour afficher les champs de la table
+# pages/page2.py
 
 import pandas as pd
 import streamlit as st
 
-def show_page2():
-    st.title("Page 2: Informations sur les Tables")
+# Titre pour la deuxième page
+st.title("Page 2: Informations sur les Tables")
 
-    # Lecture du fichier Excel pour cette page
-    field_list = pd.read_excel("Table and Field List.xlsx", sheet_name='Field List')
-    field_list['TABLE_NAME'] = field_list['TABLE_NAME'].astype(str).str.upper()
+# Lecture du fichier Excel pour cette page
+field_list = pd.read_excel("Table and Field List.xlsx", sheet_name='Field List')
+field_list['TABLE_NAME'] = field_list['TABLE_NAME'].astype(str).str.upper()
 
-    # Barre de recherche pour les tables (placée sous le graphe)
-    search_term_table = st.text_input("Rechercher une table (sous le graphe)")
+# Barre de recherche pour les tables
+search_term_table = st.text_input("Rechercher une table (sous le graphe)")
 
-    # Tableau pour la sélection de la table et l'affichage des champs
-    table_choice = st.selectbox('Choisissez une table pour afficher ses champs:', field_list['TABLE_NAME'].unique())
-    table_fields = field_list[field_list['TABLE_NAME'] == table_choice]
-    st.table(table_fields[['COLUMN_NAME', 'DATA_TYPE']])
+# Tableau pour la sélection de la table et l'affichage des champs
+table_choice = st.selectbox('Choisissez une table pour afficher ses champs:', field_list['TABLE_NAME'].unique())
+table_fields = field_list[field_list['TABLE_NAME'] == table_choice]
+st.table(table_fields[['COLUMN_NAME', 'DATA_TYPE']])
