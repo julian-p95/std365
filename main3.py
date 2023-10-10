3d4e80ce7e001fde780c52de2556b27a833ce89e
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-10 21:46:59 +0200

    c

--- main3.py
+++ main3.py
@@ -5,37 +5,30 @@ import streamlit as st
 # Titre de la page
 st.title("Analyse des Tables ERP")
 
-# Lecture du fichier Excel
+# Lecture des fichiers Excel
 d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
 
 # Gestion des valeurs NaN
 d365_tables['App module'].fillna("Non spécifié", inplace=True)
 d365_tables['Table group'].fillna("Non spécifié", inplace=True)
-d365_tables['Table\xa0type'].fillna("Non spécifié", inplace=True)
 
-# Filtrage des App modules
-filtered_tables = d365_tables[~d365_tables['App module'].isin(['SystemAdministration', 'General', 'Non spécifié'])]
-
-# Répartition par App module
-app_module_counts = filtered_tables['App module'].value_counts()
-
-# Graphique horizontal des App modules
+# Graphique à barres horizontales pour App modules
+app_module_counts = d365_tables['App module'].value_counts()
+app_module_counts = app_module_counts.drop(['Non spécifié', 'SystemAdministration', 'General'])
 fig1, ax1 = plt.subplots()
-app_module_counts.plot(kind='barh', ax=ax1, linewidth=2)
-plt.title('Répartition des App modules')
-plt.xlabel('Nombre de tables')
-plt.ylabel('App module')
-ax1.bar_label(ax1.containers[0])
+app_module_counts.plot(kind='barh', ax=ax1)
+ax1.set_title('Répartition des App modules')
+ax1.set_xlabel('Nombre de tables')
+ax1.set_ylabel('App modules')
+plt.yticks(fontsize=8)
 st.pyplot(fig1)
 
-# Répartition par Table group
+# Graphique à barres horizontales pour Table Group
 table_group_counts = d365_tables['Table group'].value_counts()
-
-# Graphique horizontal des Table groups
 fig2, ax2 = plt.subplots()
-table_group_counts.plot(kind='barh', ax=ax2, linewidth=2)
-plt.title('Répartition des groupes de tables')
-plt.xlabel('Nombre de tables')
-plt.ylabel('Table group')
-ax2.bar_label(ax2.containers[0])
+table_group_counts.plot(kind='barh', ax=ax2)
+ax2.set_title('Répartition des Table Group')
+ax2.set_xlabel('Nombre de tables')
+ax2.set_ylabel('Table Group')
+plt.yticks(fontsize=8)
 st.pyplot(fig2)
--- main4.py
+++ main4.py
@@ -2,28 +2,28 @@ import pandas as pd
 import streamlit as st
 
 # Lecture des fichiers Excel
-d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
 erp_relations = pd.read_excel("erp_all_table_relations_finalV2.xlsx", sheet_name='Sheet1')
+d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
 
 # Sélection de l'App module
 selected_app_module = st.selectbox('Sélectionnez un App module:', d365_tables['App module'].unique().tolist())
 
 # Filtre les relations par App module
-filtered_relations = erp_relations[erp_relations['App Module Parent'] == selected_app_module]
-relation_counts = filtered_relations['Table Parent'].value_counts().reset_index()
-relation_counts.columns = ['Table name', 'Total Relations']
+filtered_relations = erp_relations[(erp_relations['App Module Parent'] == selected_app_module)]
 
-# Tableau récapitulatif des relations par App module
+# Compte le nombre total de relations par App module Enfant
 relation_summary = filtered_relations.groupby(['App Module Parent', 'App Module Enfant']).size().reset_index(name='Total Relations')
 relation_summary = relation_summary.sort_values(by='Total Relations', ascending=False)
 st.table(relation_summary)
 
-# Filtre les tables par App module et ajoute le nombre total de relations
+# Filtre les tables par App module
 filtered_tables = d365_tables[d365_tables['App module'] == selected_app_module]
-filtered_tables = pd.merge(filtered_tables, relation_counts, on='Table name', how='left')
-filtered_tables['Total Relations'].fillna(0, inplace=True)
-filtered_tables = filtered_tables.sort_values(by='Total Relations', ascending=False)
 
-# Affiche les tables triées par le nombre total de relations
-table_details = filtered_tables[['Table name', 'Table label', 'Table group', 'Tabletype', 'Total Relations']]
-st.table(table_details)
+# Jointure avec le nombre total de relations
+total_relations_count = filtered_relations['Table Parent'].value_counts().reset_index()
+total_relations_count.columns = ['Table name', 'Total Relations']
+filtered_tables = pd.merge(filtered_tables, total_relations_count, on='Table name', how='left')
+
+# Trie les tables par le nombre total de relations et affiche les 20 premières
+filtered_tables = filtered_tables.sort_values(by='Total Relations', ascending=False).head(20)
+st.table(filtered_tables[['Table name', 'Table label', 'Table group', 'Tabletype', 'Total Relations']])
