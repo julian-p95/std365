f39264a6c33d14dbc8005ed4717621d1ba555c2f
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-10 21:09:50 +0200

    f

--- main3.py
+++ main3.py
@@ -11,20 +11,24 @@ d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
 # Gestion des valeurs NaN
 d365_tables['App module'].fillna("Non spécifié", inplace=True)
 d365_tables['Table group'].fillna("Non spécifié", inplace=True)
-d365_tables['Table type'].fillna("Non spécifié", inplace=True)
+d365_tables['Tabletype'].fillna("Non spécifié", inplace=True)
 
-# Graphique à barres horizontales
-grouped_counts = d365_tables['App module'].value_counts()
-fig, ax = plt.subplots()
-grouped_counts.plot(kind='barh', ax=ax)
-ax.set_title('Répartition des App modules')
-ax.set_xlabel('Nombre de tables')
-ax.set_ylabel('App modules')
-ax.set_yticks(ax.get_yticks())  # Augmentation de l'espace entre les barres
-st.pyplot(fig)
+# Supprimer les App modules indésirables
+d365_tables = d365_tables[~d365_tables['App module'].isin(['SystemAdministration', 'General', 'Non spécifié'])]
 
-# Tableau des préfixes des labels de table
-d365_tables['Table Prefix'] = d365_tables['Table label'].str.split(' ').str[0].str.upper()
-prefix_counts = d365_tables['Table Prefix'].value_counts().reset_index()
-prefix_counts.columns = ['Table Prefix', 'Count']
-st.table(prefix_counts)
+# Graphique à barres horizontales pour App module
+app_module_counts = d365_tables['App module'].value_counts()
+fig1, ax1 = plt.subplots()
+app_module_counts.plot(kind='barh', ax=ax1)
+plt.title('Répartition des App modules')
+plt.xlabel('Nombre de tables')
+plt.ylabel('App module')
+ax1.bar_label(ax1.containers[0])
+plt.tight_layout()
+st.pyplot(fig1)
+
+# Comptage des préfixes des labels des tables
+d365_tables['Prefix'] = d365_tables['Table label'].str.split().str[0].str.upper()
+prefix_counts = d365_tables['Prefix'].value_counts()
+st.table(prefix_counts.reset_index().rename(columns={'index': 'Prefix', 'Prefix': 'Count'}))
+"
--- main4.py
+++ main4.py
@@ -2,8 +2,8 @@ import pandas as pd
 import streamlit as st
 
 # Lecture des fichiers Excel
+d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
 erp_relations = pd.read_excel("erp_all_table_relations_finalV2.xlsx", sheet_name='Sheet1')
-d365_tables = pd.read_excel("/D365FO.xlsx", sheet_name='D365 Table')
 
 # Sélection de l'App module
 selected_app_module = st.selectbox('Sélectionnez un App module:', d365_tables['App module'].unique().tolist())
@@ -19,11 +19,7 @@ st.table(relation_summary)
 # Filtre les tables par App module
 filtered_tables = d365_tables[d365_tables['App module'] == selected_app_module]
 
-# Compte le nombre de relations pour chaque table
-total_relations_count = filtered_relations['Table Name Parent'].value_counts().reset_index()
-total_relations_count.columns = ['Table name', 'Total Relations']
-filtered_tables = pd.merge(filtered_tables, total_relations_count, on='Table name', how='left')
-
-# Trie les tables par le nombre total de relations et affiche les 20 premières
-sorted_tables = filtered_tables.sort_values(by='Total Relations', ascending=False).head(20)
-st.table(sorted_tables[['Table name', 'Table label', 'Table group', 'Table type', 'Total Relations']])
+# Affiche les tables triées par le nombre total de relations
+table_choice = st.selectbox('Choisissez une table pour afficher ses détails:', filtered_tables['Table name'].tolist())
+table_details = filtered_tables[filtered_tables['Table name'] == table_choice]
+st.table(table_details[['Table name', 'Table label', 'Table group', 'Tabletype']])
