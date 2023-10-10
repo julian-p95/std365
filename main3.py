871fe2195ca4f2972e2dd1df6f2c956c3fcbaf30
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-10 19:08:45 +0200

    Update main3.py

--- main3.py
+++ main3.py
@@ -6,30 +6,22 @@ import streamlit as st
 st.title("Analyse des Tables ERP")
 
 # Lecture des fichiers Excel
-erp_relations = pd.read_excel("erp_all_table_relations_finalV2.xlsx", sheet_name='Sheet1')
-d365_tables = pd.read_excel("D365FO.xlsx", sheet_name='D365 Table')
+d365_tables = pd.read_excel("/path/to/D365FO.xlsx", sheet_name='D365 Table')
+erp_relations = pd.read_excel("/path/to/erp_all_table_relations_finalV2.xlsx", sheet_name='Sheet1')
 
 # Gestion des valeurs NaN
 d365_tables['App module'].fillna("Non spécifié", inplace=True)
 d365_tables['Table group'].fillna("Non spécifié", inplace=True)
-d365_tables['Table type'].fillna("Non spécifié", inplace=True)
+d365_tables['Tabletype'].fillna("Non spécifié", inplace=True)
 
-# Calcul du nombre total de tables
-total_tables = len(d365_tables)
+# Création d'une nouvelle colonne pour App module - Table group - Table type
+d365_tables['AppModule-TableGroup-TableType'] = d365_tables['App module'] + " - " + d365_tables['Table group'] + " - " + d365_tables['Tabletype']
 
-# Création d'une nouvelle colonne pour la combinaison "AppModule - TableGroup - TableType"
-d365_tables['AppModule-TableGroup-TableType'] = d365_tables['App module'] + " - " + d365_tables['Table group'] + " - " + d365_tables['Table type']
-
-# Sélection de l'App module
-selected_app_module = st.selectbox('Sélectionnez un App module:', ['ALL'] + d365_tables['App module'].unique().tolist())
-if selected_app_module != 'ALL':
-    d365_tables = d365_tables[d365_tables['App module'] == selected_app_module]
-
-# Graphique à barres horizontales pour la combinaison "AppModule - TableGroup - TableType"
-combination_counts = d365_tables['AppModule-TableGroup-TableType'].value_counts()
+# Graphique à barres horizontales pour AppModule-TableGroup-TableType
+grouped_counts = d365_tables['AppModule-TableGroup-TableType'].value_counts()
 fig, ax = plt.subplots()
-combination_counts.plot(kind='barh', ax=ax)
-plt.title('Répartition des tables par AppModule - TableGroup - TableType')
+grouped_counts.plot(kind='barh', ax=ax)
+plt.title('Répartition des App module - Table group - Table type')
 plt.xlabel('Nombre de tables')
-plt.ylabel('AppModule - TableGroup - TableType')
+plt.ylabel('App module - Table group - Table type')
 st.pyplot(fig)
