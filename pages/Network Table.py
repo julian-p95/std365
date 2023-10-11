830126cce44ee8f21f0b6e964852dcc196b1eb98
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-11 16:53:13 +0200

    Update Network Table.py

--- pages/Network Table.py
+++ pages/Network Table.py
@@ -36,19 +36,41 @@ connected_tables = set(erp_relations.loc[erp_relations['Table Parent'] == centra
 connected_tables |= set(erp_relations.loc[erp_relations['Table Enfant'] == central_table, 'Table Parent'].tolist())
 connected_tables.add(central_table)
 
+# Widgets pour sélectionner les modules d'application
+connected_app_modules = d365_tables[d365_tables['Table name'].isin(connected_tables)]['App module'].unique().tolist()
+selected_app_modules = st.multiselect('Sélectionnez les modules d’application à afficher:', connected_app_modules, default=connected_app_modules)
+
 # Création du graphe
 net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="black")
 
+# Légende pour les modules d'application
+st.write("### Légende")
+legend = {module: app_module_colors[module] for module in selected_app_modules}
+st.write(pd.DataFrame(list(legend.items()), columns=["Module d'application", "Couleur"]))
+
 # Ajout des nœuds
+app_module_counter = {}
 for table in connected_tables:
     filtered_df = d365_tables[d365_tables['Table name'] == table]
     if not filtered_df.empty:
         table_info = filtered_df.iloc[0]
+        app_module = table_info['App module']
+        
+        if app_module not in selected_app_modules:
+            continue
+        
         title_str = "\n".join([f"{col}: {table_info[col]}" for col in table_info.index if pd.notna(table_info[col])])
-        color = app_module_colors.get(table_info['App module'], random_color())
+        color = app_module_colors.get(app_module, random_color())
         net.add_node(table, title=title_str, color=color)
+        
+        if app_module in app_module_counter:
+            app_module_counter[app_module] += 1
+        else:
+            app_module_counter[app_module] = 1
 
-# ... (le reste du code est inchangé jusqu'à l'ajout des arêtes)
+# Tableau résumé
+st.write("### Tableau résumé")
+st.write(pd.DataFrame(list(app_module_counter.items()), columns=["Module d'application", "Nombre de relations"]))
 
 # Ajout des arêtes
 existing_nodes = set(net.get_nodes())
@@ -61,9 +83,6 @@ for _, row in erp_relations.iterrows():
             net.add_edge(parent, child, title=relation)
     except Exception as e:
         st.write(f"Erreur lors de l'ajout de l'arête de {parent} à {child}: {e}")
-
-# ... (le reste du code est inchangé)
-
 
 # Affichage du graphe
 net.save_graph("temp.html")
