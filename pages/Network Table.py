6cfeff306c42ca831dc67a77ba524fdde53d6a5f
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-11 16:58:43 +0200

    Update Network Table.py

--- pages/Network Table.py
+++ pages/Network Table.py
@@ -7,7 +7,7 @@ import streamlit as st
 # Titre pour la première page
 st.title("Graphe centré sur une table")
 
-# Générer une couleur aléatoire
+# Fonction pour générer une couleur aléatoire
 def random_color():
     return "#{:02x}{:02x}{:02x}".format(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
@@ -36,41 +36,37 @@ connected_tables = set(erp_relations.loc[erp_relations['Table Parent'] == centra
 connected_tables |= set(erp_relations.loc[erp_relations['Table Enfant'] == central_table, 'Table Parent'].tolist())
 connected_tables.add(central_table)
 
-# Widgets pour sélectionner les modules d'application
+# Trouver les modules d'application connectés et créer un widget multiselect
 connected_app_modules = d365_tables[d365_tables['Table name'].isin(connected_tables)]['App module'].unique().tolist()
 selected_app_modules = st.multiselect('Sélectionnez les modules d’application à afficher:', connected_app_modules, default=connected_app_modules)
+
+# Créer la légende pour les modules d'application
+st.write("### Légende")
+legend_data = {module: app_module_colors[module] for module in selected_app_modules}
+legend_df = pd.DataFrame(list(legend_data.items()), columns=["Module d'application", "Couleur"])
+st.write(legend_df)
 
 # Création du graphe
 net = Network(height="750px", width="100%", bgcolor="#ffffff", font_color="black")
 
-# Légende pour les modules d'application
-st.write("### Légende")
-legend = {module: app_module_colors[module] for module in selected_app_modules}
-st.write(pd.DataFrame(list(legend.items()), columns=["Module d'application", "Couleur"]))
-
-# Ajout des nœuds
+# Ajout des nœuds et comptage des modules d'application
 app_module_counter = {}
 for table in connected_tables:
     filtered_df = d365_tables[d365_tables['Table name'] == table]
     if not filtered_df.empty:
         table_info = filtered_df.iloc[0]
         app_module = table_info['App module']
-        
         if app_module not in selected_app_modules:
             continue
-        
         title_str = "\n".join([f"{col}: {table_info[col]}" for col in table_info.index if pd.notna(table_info[col])])
         color = app_module_colors.get(app_module, random_color())
         net.add_node(table, title=title_str, color=color)
-        
-        if app_module in app_module_counter:
-            app_module_counter[app_module] += 1
-        else:
-            app_module_counter[app_module] = 1
+        app_module_counter[app_module] = app_module_counter.get(app_module, 0) + 1
 
-# Tableau résumé
+# Afficher le tableau résumé
 st.write("### Tableau résumé")
-st.write(pd.DataFrame(list(app_module_counter.items()), columns=["Module d'application", "Nombre de relations"]))
+summary_df = pd.DataFrame(list(app_module_counter.items()), columns=["Module d'application", "Nombre de relations"])
+st.write(summary_df)
 
 # Ajout des arêtes
 existing_nodes = set(net.get_nodes())
