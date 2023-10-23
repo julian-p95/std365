bc1ea8e02b165dbe73ccbf519964ddc7f3dcd00e
Julian Passebecq <julianpassebecq@gmail.com>
2023-10-23 13:45:43 +0200

    1

---
+++ pages/Table_Steeve.py
@@ -0,0 +1,22 @@
+# pages/page2.py
+
+import pandas as pd
+import streamlit as st
+
+# Titre de la page
+st.title("Page 2: Informations sur les Tables")
+
+# Lecture du fichier Excel
+# Changer le nom du fichier et la feuille si nécessaire
+field_list = pd.read_excel("Table_Steeve.xlsx")  
+field_list['TableName'] = field_list['TableName'].astype(str).str.upper()  # Convertir en majuscules
+
+# Barre de recherche pour les tables
+# Utilisez cette fonction si vous souhaitez ajouter une fonction de recherche
+search_term_table = st.text_input("Rechercher une table (sous le graphe)")
+
+# Sélection de la table et affichage des champs
+# Remplacer 'TABLE_NAME' et 'COLUMN_NAME', 'DATA_TYPE' par les noms des colonnes correspondants
+table_choice = st.selectbox('Choisissez une table pour afficher ses champs:', field_list['TableName'].unique())
+table_fields = field_list[field_list['TableName'] == table_choice]
+st.table(table_fields[['FieldName']])
